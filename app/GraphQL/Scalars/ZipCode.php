20f5309369c3b799d52758c00811e1da10ca9c75
Victor Elias Barrera Florez <vbarrera@merqueo.com>
2021-12-02 16:16:57 -0500

    refactor: Modificación y pruebas de scala

---
+++ app/GraphQL/Scalars/ZipCode.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\GraphQL\Scalars;
+
+use GraphQL\Type\Definition\ScalarType;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * Read more about scalars here https://webonyx.github.io/graphql-php/type-definitions/scalars
+ */
+class ZipCode extends ScalarType
+{
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        // Assuming the internal representation of the value is always correct
+        return strtoupper($value);
+
+        // TODO validate if it might be incorrect
+    }
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function parseValue($value)
+    {
+        // TODO implement validation
+Log::debug("parseValue",[$value]);
+        return '-'.$value;
+    }
+
+    /**
+     * Parses an externally provided literal value (hardcoded in GraphQL query) to use as an input.
+     *
+     * E.g.
+     * {
+     *   user(email: "user@example.com")
+     * }
+     *
+     * @param  \GraphQL\Language\AST\Node  $valueNode
+     * @param  array<string, mixed>|null  $variables
+     * @return mixed
+     */
+    public function parseLiteral($valueNode, ?array $variables = null)
+    {
+        // TODO implement validation
+        Log::debug("parseValue",[$valueNode]);
+        return $valueNode->value;
+    }
+}
